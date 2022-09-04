cd408d24cb9ddca7a3a20162a619a0c3adf2a3b6
Markus Doan-Le <markus.doanle@gmail.com>
2022-09-04 20:03:14 +0000

    added comments on oddoreven function

--- javascript-loops-practice/find-index.js
+++ javascript-loops-practice/find-index.js
@@ -15,3 +15,16 @@ function findIndex(array, value) {
     return indexOfValue;
   }
 }
+
+/*
+1)Is the value in the given list of numbers?
+2) If it is, say that.
+3) If its not, then say that.
+
+PC
+1) counter variable
+2) for loop to iterate through array
+3) if statement to check if the current index is equal to the value
+  a) If it is, then state the index where the sought value was found
+  b)
+*/
--- javascript-loops-practice/odd-or-even.js
+++ javascript-loops-practice/odd-or-even.js
@@ -10,3 +10,15 @@ function oddOrEven(numbers) {
   }
   return resultsArray;
 }
+
+/*
+1) search through list
+2) Is the number odd? even?
+3) tell me the answer
+
+PC
+1) Output array
+2)for loop
+3) if statement (if array[i] % 2 ===0 then add even, if not then add odd)
+4)return output array.
+*/
