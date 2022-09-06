57a8774309f841cad5503a20a1db38179a4f65f8
Markus Doan-Le <markus.doanle@gmail.com>
2022-09-06 02:01:42 +0000

    completed array-manipulation-1 exercise

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,10 @@
 /* exported compact */
+function compact(array) {
+  var returnArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (!array[i] === false) {
+      returnArray.push(array[i]);
+    }
+  }
+  return returnArray;
+}
