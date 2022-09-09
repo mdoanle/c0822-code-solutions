117c4b5a25c6063a7ac107d6ce6b5ad39b82e4d7
Markus Doan-Le <markus.doanle@gmail.com>
2022-09-09 17:54:22 +0000

    corrected capitalize-word function

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,11 +1,7 @@
 /* exported capitalizeWord */
 function capitalizeWord(word) {
   if (word.toLowerCase() === 'javascript') {
-    var firstLetterJava = word[0].toUpperCase();
-    var restOfJava = word.substring(1, 4).toLowerCase();
-    var jsScript = word[4].toUpperCase();
-    var restOfScript = word.substring(5).toLowerCase();
-    return firstLetterJava + restOfJava + jsScript + restOfScript;
+    return 'JavaScript';
   } else {
     var firstBigLetter = word[0].toUpperCase();
     var restOfWord = word.substring(1).toLowerCase();
