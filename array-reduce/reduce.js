f35b7855bc633158f0b9727a97bd698a1ef17362
Markus Doan-Le <markus.doanle@gmail.com>
2022-10-20 21:12:54 +0000

    fixed reduce function in challenge

--- array-reduce/reduce.js
+++ array-reduce/reduce.js
@@ -2,17 +2,22 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const sumReducer = (acc, curr) => acc + curr;
 
-const sum = reduce(numbers, sumReducer, 0);
+const sum = reduce(numbers, sumReducer);
 
 function reduce(array, reducer, initialValue) {
   if (initialValue === undefined) {
-    const accumulator = 0;
+    let accumulator = array[0];
+    for (let i = 1; i < array.length; i++) {
+      accumulator = reducer(accumulator, array[i]);
+    }
+    return accumulator;
+  } else {
+    let accumulator = initialValue;
+    for (let i = 0; i < array.length; i++) {
+      accumulator = reducer(accumulator, array[i]);
+    }
+    return accumulator;
   }
-  let accumulator = initialValue;
-  for (let i = 0; i < array.length; i++) {
-    accumulator = reducer(accumulator, array[i]);
-  }
-  return accumulator;
 }
 
 console.log(sum);
