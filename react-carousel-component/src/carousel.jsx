cd75e1ec374e6c330f25a61ea91dcdd648e0e75d
Markus Doan-Le <markus.doanle@gmail.com>
2022-10-27 21:16:46 +0000

    React Carousel in progress pt 2

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,1 +1,20 @@
-// import React from 'react';
+import React from 'react';
+
+class Carousel extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <i></i>
+          <img></img>
+          <i></i>
+        </div>
+        <div>
+          <i></i>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
