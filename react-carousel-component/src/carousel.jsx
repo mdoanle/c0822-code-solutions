84ee06e142be8da21160a01cb767cf5e2d73f7d4
Markus Doan-Le <markus.doanle@gmail.com>
2022-10-30 00:54:18 +0000

    completed react-carousel-component exercise

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,16 +1,87 @@
 import React from 'react';
 
 class Carousel extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSlide: 0
+    };
+    this.handleClickLeft = this.handleClickLeft.bind(this);
+    this.handleClickRight = this.handleClickRight.bind(this);
+    this.handleDotClick = this.handleDotClick.bind(this);
+  }
+
+  handleClickRight() {
+    clearInterval(this.state.intervalId);
+    const currentSlide = this.state.currentSlide;
+    if (this.state.currentSlide < this.props.drakeImages.length - 1) {
+      this.setState({
+        currentSlide: currentSlide + 1
+      });
+    } else {
+      this.setState({
+        currentSlide: 0
+      });
+    }
+    this.slideTimer();
+  }
+
+  handleClickLeft() {
+    clearInterval(this.state.intervalId);
+    const currentSlide = this.state.currentSlide;
+    if (currentSlide > 0) {
+      this.setState({
+        currentSlide: currentSlide - 1
+      });
+    } else {
+      this.setState({
+        currentSlide: this.props.drakeImages.length - 1
+      });
+    }
+    this.slideTimer();
+  }
+
+  renderDots() {
     return (
-      <div>
-        <div>
-          <i></i>
-          <img></img>
-          <i></i>
+      this.props.drakeImages.map((value, index) => {
+        const dotClass = index === this.state.currentSlide ? 'solid' : 'regular';
+        return <i key={index} className={`fa-${dotClass} fa-circle`} index={index} onClick={this.handleDotClick}></i>;
+      })
+    );
+  }
+
+  handleDotClick(event) {
+    const index = event.target.getAttribute('index');
+    clearInterval(this.state.intervalId);
+    this.setState({
+      currentSlide: Number(index)
+    });
+    this.slideTimer();
+  }
+
+  slideTimer() {
+    const intervalId = setInterval(this.handleClickRight, 3000);
+    this.setState({
+      intervalId
+    });
+  }
+
+  componentDidMount() {
+    this.slideTimer();
+  }
+
+  render() {
+    const drakeImages = this.props.drakeImages;
+    const currentSlide = this.state.currentSlide;
+    return (
+      <div className='container'>
+        <div className='carousel-container'>
+          <i onClick = {this.handleClickLeft} className="fa-solid fa-chevron-left"></i>
+          <img src={drakeImages[currentSlide]}></img>
+          <i onClick = {this.handleClickRight} className="fa-solid fa-chevron-right"></i>
         </div>
-        <div>
-          <i></i>
+        <div className='dot-container'>
+          {this.renderDots()}
         </div>
       </div>
     );
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Carousel from './carousel';
 
-/* const drakeImages = [
+const drakeImages = [
   'https://media.gettyimages.com/photos/drake-attends-the-premiere-of-hbos-euphoria-at-the-cinerama-dome-on-picture-id1153762018?s=612x612',
   'https://hiphop24x7.com/wp-content/uploads/2022/05/New-Video-Future-Ft.-Drake-Tems-WAIT-FOR-U.png',
   'https://i.pinimg.com/564x/e4/42/4a/e4424a4d01194bff7564996f8c37625e.jpg',
   'https://preview.redd.it/xhu2gmxr58j81.jpg?width=640&crop=smart&auto=webp&s=e3b7571dba6e87d8fa426f27ca6be317baf6d2f2',
   'https://i.pinimg.com/736x/c6/fa/31/c6fa31681e558aa5740814281094d11c.jpg'
-]; */
+];
 
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
-root.render(<Carousel />);
+root.render(<Carousel drakeImages={drakeImages} />);
