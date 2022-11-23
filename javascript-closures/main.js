7ed1d02fdd7d598c87af90fa71c9f9908c62cbbd
Markus Doan-Le <markus.doanle@gmail.com>
2022-11-23 22:32:27 +0000

    completed javascript-closures exercise

---
+++ javascript-closures/main.js
@@ -0,0 +1,11 @@
+function graduate(credential) {
+  return function (fullName) {
+    return `${fullName}, ${credential}`;
+  };
+}
+
+const medicalSchool = graduate('M.D.');
+const lawSchool = graduate('Esq.');
+
+console.log(medicalSchool('Markus Doan-Le'));
+console.log(lawSchool('Markus Doan-Le'));
